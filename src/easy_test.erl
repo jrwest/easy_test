7de7d103f0e44070484e92de3b30f7f8861bed5a
Jordan West <jordanrw@gmail.com>
2011-03-27 14:14:36 -0700

    some more refactoring

--- src/easy_test.erl
+++ src/easy_test.erl
@@ -11,12 +11,15 @@
 -export([parse_transform/2]).
 
 -define(EASY_TEST_PREFIX, "test_").
+-define(EASY_TEST_DEFAULT_GRP_OPTS, [shuffle]).
+
+-define(EASY_TESTS_ETS, easy_exports).
 
 parse_transform(Forms, _) ->
-    ets:new(easy_test, [ordered_set, protected, named_table]),
+    ets:new(?EASY_TESTS_ETS, [ordered_set, protected, named_table]),
     scan_forms(Forms),
     Result = rewrite(Forms),
-    ets:delete(easy_test),
+    ets:delete(?EASY_TESTS_ETS),
     Result.
 
 scan_forms(Forms) ->
@@ -28,9 +31,9 @@ scan_forms(Forms) ->
 
 form({attribute, _L, easy_test, Data},  _) ->
     Name = proplists:get_value(test, Data),
-    HasInit = proplists:get_value(has_config, Data),
+    HasConfig = proplists:get_value(has_config, Data),
     store_export(Name,1),
-    case HasInit of
+    case HasConfig of
 	true ->
 	    store_export(Name, 0);
 	_ ->
@@ -48,7 +51,7 @@ form(_, _) ->
     skipped.
 
 store_export(Name,Arity) ->
-    ets:insert(easy_test, {make_ref(), Name, Arity}).
+    ets:insert(?EASY_TESTS_ETS, {make_ref(), Name, Arity}).
 
 rewrite([{attribute, _, module, _Name}=M | Fs]) ->
     module_decl(M, Fs);
@@ -67,7 +70,7 @@ rewrite([], As, {ExportAllFun, Tests}) ->
 	true -> As end, ExportAllFun}.
 
 module_decl(M, Fs) ->
-    {AllExports, Tests} = fetch_data(ets:first(easy_test), [], []),
+    {AllExports, Tests} = fetch_data(ets:first(?EASY_TESTS_ETS), [], []),
     {Fs1, ExportAllFun} = rewrite(Fs, [], {true, Tests}),
     Es = if ExportAllFun -> [{all, 0} | AllExports];
 	    true -> AllExports end,
@@ -76,13 +79,13 @@ module_decl(M, Fs) ->
 fetch_data('$end_of_table', AllAcc, TestsAcc) ->
     {lists:reverse(AllAcc), lists:reverse(TestsAcc)};
 fetch_data(Key, AllAcc, TestsAcc) ->
-    [{_, Name, Arity} | _] = ets:lookup(easy_test, Key),
+    [{_, Name, Arity} | _] = ets:lookup(?EASY_TESTS_ETS, Key),
     Data = {Name, Arity},
     case Arity of
 	0 ->
-	    fetch_data(ets:next(easy_test, Key), [Data | AllAcc], TestsAcc);
+	    fetch_data(ets:next(?EASY_TESTS_ETS, Key), [Data | AllAcc], TestsAcc);
 	1 ->
-	    fetch_data(ets:next(easy_test, Key), [Data | AllAcc], [Data | TestsAcc])
+	    fetch_data(ets:next(?EASY_TESTS_ETS, Key), [Data | AllAcc], [Data | TestsAcc])
     end.
 
 write_all(As, Tests) ->
